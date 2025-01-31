3e387812008a6233d3fea5e6f279e1d08ccba4e2
Sankalp Kaushik <sskaushi@ucsd.edu>
2025-01-31 13:33:21 -0800

    Added setup.py and debugged pyproject file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="InteGraphAD",
+    version="0.1.0",
+    description="A CSDL-based numerical integration package",
+    author="Sankalp Kaushik",
+    author_email="kaushiksankalp02@gmail.com",
+    packages=find_packages(where="integraphad"),
+    install_requires=[
+        "numpy",
+        "csdl-alpha",
+    ],
+    include_package_data=True,
+    license="MIT",
+)
